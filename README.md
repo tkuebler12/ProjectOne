52665dbc69a1ca414f8a51474aabc368ffe19280
Enrique Collazo <ecollazo1991@gmail.com>
2021-01-27 22:01:05 -0500

    updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProjectOne
 First project for BCS
+
+A fully working site that allows the user to select food sensitivities and/or ingredients which will return a result of recipes and/or restaurants' menu items that leave out the selected allergens/sensitivities and/or ingredients through open source APIs to retreive information from recipes and items off menus of restuarants.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var queryURL = "https://api.edmunds.com/api/editorial/v3/makes/{honda}/models/{accord}/years/{2020}/expertcontent?api_key={e62194363dmsh3cc1238e4b3eee1p1b8ab4jsn101d84f87af9}"
 
 ajax({
@@ -8,3 +9,24 @@ ajax({
     console.log(response);
 
 });
+=======
+$(document).ready(function () {
+​
+    // Food Database API
+​
+    var ingr = ""
+    // var APP_ID = "cdc2f678"
+    // var APP_KEY = "2ccb5f96df52396e065d26ba486dcc0a -"
+    var queryURL = 'https://api.edamam.com/search?app_id=cdc2f678&app_key=2ccb5f96df52396e065d26ba486dcc0a&q=pizza';
+​
+    $.ajax({
+        URL: queryURL,
+        method: "GET",
+    })
+    .then(function (response) {
+        console.log(response);
+​
+    });
+​
+});
+>>>>>>> bf723d860f06b1ab115928b898c82f062315ea37
