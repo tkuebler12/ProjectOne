a835bd12ec5d1e68a8c5757b9d83a4cf13a4d892
Enrique Collazo <ecollazo1991@gmail.com>
2021-02-02 19:46:35 -0500

    updated the jQuery

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 $(document).ready(function () {
 
 
+
+    // Run Recipe search on this click event
+    $(".RecipeSearchBtn").on("click", function(){
+        recipeResponse();
+
+    });
+    
+    function recipeResponse(){
+
     // Recipe Search
-    var searchedFood = "pizza";
-    var api_id = "cdc2f678";
-    var app_key = "2ccb5f96df52396e065d26ba486dcc0a";
+    // var api_id = "cdc2f678";
+    // var app_key = "2ccb5f96df52396e065d26ba486dcc0a";
+    var searchedFood = $('.RecipeSearchInput').val().trim();
     var queryURL = "https://api.edamam.com/search?q=" + searchedFood + "&app_id=cdc2f678&app_key=2ccb5f96df52396e065d26ba486dcc0a";
+    
+    localStorage.clear();
+    localStorage.setItem("food", searchedFood);
+
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function(response){
-        console.log("Recipe Search");
-        console.log("Searched Text", response.q)
-        console.log(response);
-    })
+        console.log("----------------------------------------")
+        console.log("Recipe", response);
+        console.log("----------------------------------------")
+
+        var ingredientListObj = response.hits[0].recipe.ingredients;
+
+        for (var i = 0; i < ingredientListObj.length; i++) {
+            var recipeName = response.q;
+            var ingredient = ingredientListObj[i].text;
+            $(".recipe-name").text(recipeName);
+            $(".ingredientList").prepend("<li>" + ingredient + "<li>");
+            $(".expandBtn").text("Expand for " + searchedFood + " Ingredients");
+        };
+
+    });
+
+    };
 
 
     // Nutrition Search
-    var nutritionSearch = "dragon%20fruit";
+    /*var nutritionSearch = "dragon%20fruit";
     var api_id = "186ab126";
     var app_key = "be84bbda46e01041f055652ea7ba013d";
     var queryURL = "https://api.edamam.com/api/nutrition-data?ingr=" + nutritionSearch + "&app_id=186ab126&app_key=be84bbda46e01041f055652ea7ba013d";
@@ -27,7 +53,7 @@ $(document).ready(function () {
     }).then(function(response){
         console.log("Nutrition API");
         console.log(response);
-    })
+    })*/
 
 
     // Food Database
@@ -39,15 +65,8 @@ $(document).ready(function () {
         url: queryURL,
         method: "GET",
     }).then(function(response){
-        console.log("Food Database");
-    
-        console.log("Search Text", response.text);
-        var name = response.text;
         console.log("----------------------------------------")
-        console.log(name);
-        console.log("Nutritents", response.parsed[0].food.nutrients);
-        console.log("Measurements", response.hints);
-        console.log("Main 'Response'", response);
+        console.log("Food Database", response);
         console.log("----------------------------------------")
     })
 
